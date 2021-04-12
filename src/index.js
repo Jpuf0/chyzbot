e0be635a4bf2970b971d86c94b219dbcaca943f9
Jpuf0 <Jpuffthebeast@gmail.com>
2021-04-13 00:52:41 +0100

    javascript module fuckery

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import discord from "discord.js";
 import logger from "./utils/Logger.js"
-import config from "./config"
+import config from "./config.js"
 
-const client = discord.Client();
+const client = new discord.Client();
 
 client.login(config.bot.token)
